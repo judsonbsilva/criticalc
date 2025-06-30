d802b7a25796ff03653e4677d7815c03f5a5a93f
Judson Barroso <judsonbarroso@gmail.com>
2025-06-30 11:50:23 -0300

    fix(drug drop-down): corrige o dropdown para desktop e mobile, mostrando as propriedades da Drug apenas quando clicada

--- src/components/Drug.vue
+++ src/components/Drug.vue
@@ -46,11 +46,9 @@ const moveUp = () => {
     }, 1000);
     
 };
-const show = () => {
+const toggleActive = () => {
+    console.log('toggleActive', isActive.value);
     isActive.value = !isActive.value;
-//    calcDrug(props.drugData, props.weight, props.height, props.tag);
- //   showCalc();
- //   moveUp();
 };
 
 /*
@@ -68,23 +66,86 @@ is-dark
 //const calc = ref(0);
 </script>
 <template>
-    <div :class="['grid', 'has-3-cols-desktop', 'has-3-cols-mobile', 'drug-box', isActive ? 'active': '']" @click="show">
-        <div class="cell">
+    <div :class="['grid', 'has-3-cols-desktop', 'has-3-cols-mobile', 'drug-box', isActive ? 'active': '']">
+        <div class="cell is-col-2-mobile">
             <div class="card drug-card">
                 <div class="card-content">
-                    <div class="content has-text-white">
+                    <div class="content has-text-white" @click="toggleActive">
                         <b class="drug-name">{{ drugData.NOME }}</b>
                         <p class="drug-name">{{ drugData.FANTASIA_APRESENTACAO }}</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="cell is-gap-1">
+        <div class="cell is-col-2-mobile">
             <div class="box drug-card">
                 <div class="tags">
                     <span v-for="oneTag in drugData.TAGS" :class=colorTag[oneTag]>{{ oneTag }}</span>
                 </div>
-                <font-awesome-icon class="button-drop" :icon="['fas', 'arrow-right']" />
+                <font-awesome-icon class="button-drop" :icon="['fas', 'arrow-right']" @click="toggleActive"/>
+            </div>
+        </div>
+        <div class="cell cell is-col-span-2" v-if="isActive">
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+                        <b>Indicação</b>
+                        <p>{{ drugData.RESUMO_INDICACAO }}</p>
+                        <b>Dose usual</b>
+                        <p>{{ drugData.DOSE_01 }} {{ drugData.UNI_APRESENTACAO }}/{{ drugData.UNI_01 }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cell cell is-col-span-2"  v-if="isActive">
+            <div class="card has-background-primary-100 has-text-primary-invert" v-if="weight">
+                <div class="card-content">
+                    <div class="grid">
+                        <div class="cell">
+                            <b>Diluição</b>
+                            <p>{{ drugData.DILUICAO }}</p>
+                        </div>
+                        <div class="cell">
+                            <b>Fazer</b>
+                            <p>{{ showCalc(drugData, calcDrug(drugData, weight)) }}</p>
+                        </div>
+                    </div>
+                    <div class="grid" v-if="drugData.ADMINISTRACAO == 'BIC'">
+                        <div class="cell">
+                            <div class="content">
+                                <b>Cálculo de dose</b>
+                                <input class="input" type="number" v-model="vazao" placeholder="vazão atual (ml/h)" />
+                            </div>
+                        </div>
+                        <div class="cell">
+                            <div class="content" v-if="vazao">
+                                <b>Dose atual</b>
+                                <p>{{ calcDrug(drugData, weight, vazao, 1) }} {{ drugData.UNI_APRESENTACAO }}/{{
+                                    drugData.UNI_01 }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-content" v-if="drugData.DOSE_02 && drugData.PAGINA == 'psa'">
+                    <div class="content">
+                        <b>Repetir (se necessário)</b>
+                        <p>Em {{ drugData.TEMPO_REPETIR }} minutos</p>
+                        <b>Fazer (dose extra)</b>
+                        <p>{{ showCalc(drugData, calcDrug(drugData, weight, NaN, 2)) }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card card has-background-primary-100 has-text-primary-invert" v-else="weight">
+                <div class="card-content">
+                    <div class="content"  @click=moveUp>
+                        <p>
+                            <span class="icon is-small">
+                                <font-awesome-icon :icon="['fas', 'arrow-up']" />
+                            </span>
+                            <span>Digite o peso (na barra do topo) para ver as doses</span>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -95,7 +156,7 @@ is-dark
     border-bottom: 1px solid #aaa;
     padding: 0px;
     margin: 0px !important;
-    cursor: pointer;
+    position:relative;
 }
 
 .drug-card {
@@ -107,16 +168,17 @@ is-dark
 .active .drug-name  {
     color: #009cbf;
     transition: color 0.5s ease-in-out;
+    cursor: pointer;
 }
 
 .button-drop {
     color: #fff;
     cursor: pointer;
     position: absolute;
-    float:right;
     right: 0.5rem;
-    margin-top: -2rem;
+    top: 0.5rem;
     padding: 0.5rem;
+    z-index:2;
 }
 
 .active .button-drop {
